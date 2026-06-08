245891f5995d0a629858f08b36491abc92bf4205
kidd <1196736083@qq.com>
2026-06-09 00:00:57 +0800

    chore(config): 优化配置和调整包结构
    - 修改默认激活配置为 local 环境
    - 添加 application-dev.yml 数据库连接池配置
    - 移除 Application 启动类中数据源自动配置排除注解
    - 将测试类迁移到 io.github.yiyongbo.scaffold 包内
    - .gitignore 中添加 application-local.yml 忽略规则

--- src/main/java/io/github/yiyongbo/scaffold/Application.java
+++ src/main/java/io/github/yiyongbo/scaffold/Application.java
@@ -2,7 +2,6 @@ package io.github.yiyongbo.scaffold;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动类
@@ -10,10 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author kidd
  * @since 2026/05/28 16:18
  */
-// TODO 暂时排除数据源自动配置，后续在配置数据源
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class
-})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
